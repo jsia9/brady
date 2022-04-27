bcef5234016b9cdcddf9b675f2e4812a311f2cb5
js <25248608+jsia9@users.noreply.github.com>
2022-04-27 14:49:20 +0100

    test rg and storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "UK South"
 }
 
 resource "azurerm_storage_account" "example" {
@@ -8,15 +8,16 @@ resource "azurerm_storage_account" "example" {
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
-    environment = "staging"
+    brady = "interview"
+    environment = "dev"
   }
 }
 
-module "rg" {
-  source = "./modules/rg"
+module "RG" {
+  source = "./modules/RG"
   rgname = var.rgname
   location = var.location
 }
