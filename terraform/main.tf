796f1390510a70c18d3721c014adcb9ed653ef36
js <25248608+jsia9@users.noreply.github.com>
2022-04-28 22:44:04 +0100

    using azure webapp

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,19 @@ resource "azurerm_resource_group" "brady" {
 }
 
 
-
-resource "azurerm_static_site" "jmlbrady" {
-  name                = "jmlbrady"
+resource "azurerm_service_plan" "bradyweather" {
+  name                = "bradyweather"
   resource_group_name = azurerm_resource_group.brady.name
-  location            = "West Europe"
+  location            = "UK South"
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
 
+resource "azurerm_linux_web_app" "bradyweatherjml" {
+  name                = "bradyweatherjml"
+  resource_group_name = azurerm_resource_group.brady.name
+  location            = azurerm_service_plan.bradyweather.location
+  service_plan_id     = azurerm_service_plan.bradyweather.id
+
+  site_config {}
 }
