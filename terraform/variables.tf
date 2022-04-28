7e9cb4ee2c68f03ea05e207d18567600dfabf9d4
js <25248608+jsia9@users.noreply.github.com>
2022-04-28 20:34:15 +0100

    tf format update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,10 @@
 variable "rgname" {
   description = "Resource Group Name"
-  default  = "brady"
-  type = string
+  default     = "brady"
+  type        = string
 }
 variable "location" {
   description = "Azure Location"
-  default = "West Europe"
-  type = string
+  default     = "West Europe"
+  type        = string
 }
