462e6f8acd6534f5b055557176700ec88c99696b
js <25248608+jsia9@users.noreply.github.com>
2022-04-29 16:22:07 +0100

    using terraform cloud for state mgmt

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,19 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.3.0"
     }
   }
+  cloud {
+    organization = "bradyjamal"
+    workspaces {
+      name = "brady"
+    }
+  }
 }
+
 
 provider "azurerm" {
   features {}
